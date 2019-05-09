50c52fa97806dc40303e46ba801dd3ae8014debe
Jeffrey Heller <heller.jeffrey@gmail.com>
2019-05-09 19:42:22 -0400

    Forecast feature working - to do => style and make data easier to read

--- src/components/ForecastContainer/ForecastContainer.js
+++ src/components/ForecastContainer/ForecastContainer.js
@@ -1,24 +1,23 @@
-// plan out 2nd api call based on App.state.data or App.fetchWeather()
-
-// design container to request/hold forecast data returned
-
-// design UI components to display forecast (Forecast window)
-
 import React, { Component } from 'react'
 import Container from 'react-bootstrap/Container'
 import Badge from 'react-bootstrap/Badge'
 import axios from 'axios'
 import ForecastItem from '../ForecastItem.js/ForecastItem'
+import ListGroup from 'react-bootstrap/ListGroup'
+import Button from 'react-bootstrap/Button'
 
 class ForecastContainer extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      data: null
+      data: null,
+      showing: 5
     }
   }
+
   componentDidMount() {
     let key = process.env.REACT_APP_WEATHER_API_KEY
+    console.log('api called')
     axios
       .get(
         'https://api.openweathermap.org/data/2.5/forecast?' +
@@ -30,15 +29,30 @@ class ForecastContainer extends Component {
   }
 
   render() {
-    console.log(this.state.data)
     return (
       <Container>
         <h3>Hourly Forecast</h3>
-        {this.state.data
-          ? this.state.data.list.map(item => (
-              <ForecastItem key={item.dt} data={item} />
-            ))
-          : null}
+        <ListGroup>
+          {this.state.data
+            ? this.state.data.list.map((item, i) => {
+                if (i < this.state.showing) {
+                  return (
+                    <ListGroup.Item key={item.dt}>
+                      <ForecastItem data={item} />
+                    </ListGroup.Item>
+                  )
+                }
+              })
+            : null}
+        </ListGroup>
+        <div className="d-flex justify-content-center mt-3">
+          <Button
+            onClick={() => this.setState({ showing: this.state.showing + 5 })}
+            variant="success"
+          >
+            Load More
+          </Button>
+        </div>
       </Container>
     )
   }
